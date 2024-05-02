089df978f4b651069084a3fb5da95e371418b31d
IdemudiaMahie <idemudiamahie23@gmail.com>
2024-05-02 12:25:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// const button = document.querySelector('button')
+// const menu = document.querySelector('.menu')
+
+// button.addEventListener('mouseover', ()=>{
+//     // menu.setAttribute('class', 'block')
+//     // menu.classList = 'block'
+//     menu.classList.add('block')
+// })
+
+// button.addEventListener('mouseout', ()=>{
+//     // menu.setAttribute('class', 'block')
+//     // menu.classList = 'block'
+//     menu.classList.remove('block')
+// })
+
+// button.ontoggle(
+//     button.addEventListener('mouseover', ()=>{
+//         // menu.setAttribute('class', 'block')
+//         // menu.classList = 'block'
+//         menu.classList.add('block')
+//     }),
+//     button.addEventListener('mouseout', ()=>{
+//         // menu.setAttribute('class', 'block')
+//         // menu.classList = 'block'
+//         menu.classList.remove('block')
+//     })
+// )
